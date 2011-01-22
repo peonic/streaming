df11b8cc59004689b92a4e0232192fa293818076
peter innerhofer <peonic@wirdorange.org>
2011-01-22 17:30:11 +0100

    updated osc_test patch, stream number implemented, know you can choose the stream from pd

--- pygst-modules/main.py
+++ pygst-modules/main.py
@@ -18,32 +18,22 @@ from video_src_udp_sink import VideoSrcToUDPSink
 from udp_src_to_v4l2loopback import UDPSrcToV4l2Loopback
 from video_src_file_sink import VideoSrcToFileSink
 
-class BaseStreaming:
+class StreamingApplication:
   """
-  Base Streaming Class:
-
-  Template Class for Gstreamer application. Basic functions for controlling, message handling 
-  pipeline parsing and viewing.
+  Streaming Application
+  
+  Class for handling Gstreamer Pipelines. 
+  
+  try: 
+  !! osc_test.pd file!!
 
   Controlling the pipeline can be done with 3 diffrent methods. 
     * commandline input
     * keyboard input in the gtk window
     * send OSC messages
+    !! Currently just OSC is implemented, see the osc_test.pd file !!
 
-  until now only basic controll function are implemented:
-    's' for start/stop (OSC message '1' or '0')
-    'q' for quit
-    'f' for fullscreen (only gtk applications)
-
-  OSC: port: 7780 messagestring: /startstopstream
-  TODO: close udp socket!!!!
-
-  Message Handling:
-  it is very important that basic message handling functions are implemented, 
-  specially for writing files on hdd. a multiplexer (z.b avimux) or the filesink need
-  a EOS (EndOfStream) message for closing the files properly.
-
-  pipeline can be parsed as they are in commandline 
+  OSC: port: 8000 messagestring: 
   """
 
   def __init__(self):  
@@ -52,6 +42,7 @@ class BaseStreaming:
     # print self.config.sections()
     
     self.pipeline_array = []
+    self.pipeline_number = 0
 
   def init_OSC(self):    
     self.osc_server = oschandler.OSCHandler((self.config.get("OSC","ServerAddress"),self.config.getint("OSC","Port")))
@@ -60,6 +51,7 @@ class BaseStreaming:
     self.osc_server.addMsgHandler(self.config.get("OSC","PrintCaps"),self.osc_print_caps)
     self.osc_server.addMsgHandler(self.config.get("OSC","StreamFactoryCreate"),self.pipeline_factory)
     self.osc_server.addMsgHandler(self.config.get("OSC","StreamDelete"),self.delete_pipeline)
+    self.osc_server.addMsgHandler(self.config.get("OSC","StreamChangeNumber"),self.change_pipeline_number)
     self.osc_server.start()
 
   def factory(self, className, args):
@@ -75,13 +67,12 @@ class BaseStreaming:
     self.window.show_all()
 
   def pipeline_factory(self,addr, tags, data, source):
-    print "recieved osc message: /stream/create/fromfactory, data: %s" % data[0]
+    print "recieved osc message: /stream/create/fromfactory, data: %s" % data[-1]
+    
     # TODO: test if Class exists!
-    #aClass = getattr(__import__(__name__),data[-1])
-    #p = aClass(self.config)
-    #self.pipeline_array.append(p)
+    
     self.pipeline_array.append(self.factory(data[0], self.config))
-    self.pipeline_array[-1].create_pipeline(len(self.pipeline_array)-1)      
+    self.pipeline_array[-1].create_pipeline(self.pipeline_number)      
 
   def parse_pipeline(self,gst_pipeline_string):
     self.pipeline_array.append(gstreamerpipeline.Pipeline(self.config))
@@ -92,6 +83,10 @@ class BaseStreaming:
     for d in data:
       print d
     #self.parse_pipeline(data[0])
+
+  def change_pipeline_number(self, addr, tags, data, source):
+    print "\nrecieved /stream/change/number: %s  \n" % data[-1] 
+    self.pipeline_number = data[0]
     
   def run(self):
     print "initializing Object"
@@ -108,7 +103,7 @@ class BaseStreaming:
     #self.pipeline_array[-1].create_pipeline(0,"/videos/test.avi")
     
     #self.pipeline_array.append(video_src_udp_sink.VideoSrcToUDPSink(self.config))
-    #self.pipeline_array[-1].create_pipeline(0)
+    #self.pipeline_array[-1].create_pipeline(0)	
     
     #self.pipeline_array.append(udp_src_to_v4l2loopback.UDPSrcToV4l2Loopback(self.config))
     #self.pipeline_array[-1].create_pipeline(0)
@@ -121,7 +116,7 @@ class BaseStreaming:
 	  self.init_OSC()
 	
     #self.ready()
-    #self.StartStop()
+    #self.StartStopAll()
     
     if self.config.getint("GTK","Init"):
       print "GTK Window initialized"
@@ -135,18 +130,17 @@ class BaseStreaming:
     print "exit"
 
   def start_stop_stream(self,addr, tags, data, source):
-    print "received new osc msg from %s" % OSC.getUrlStr(source)
-    print "with addr : %s" % addr
-    print "typetags : %s" % tags
-    print "the actual data is :%s" % data
-    print "---"
-    if 0 <= data[0] and data[0] <= 1:
-      self.ready()
-      self.StartStop()
+    print "received osc msg /stream/start from %s" % OSC.getUrlStr(source)
+    #self.ready()
+    self.StartStop(data[-1])
    
-  def StartStop(self):
+  def StartStopAll(self):
     for p in self.pipeline_array:
       p.StartStop()
+  
+  def StartStop(self, p_nr):
+    if p_nr >= 0 and p_nr < len(self.pipeline_array):
+      self.pipeline_array[p_nr].StartStop()
     
   def ready(self):
     for p in self.pipeline_array:
@@ -176,18 +170,19 @@ class BaseStreaming:
     print event.state
     print event.keyval
     if event.keyval == 115:
-      self.StartStop()
+      self.StartStopAll()
     if event.keyval == 102:
       self.Fullscreen()
     if event.keyval == 113:
       self.OnQuit(self.window)
       
   def osc_print_caps(self,addr, tags, data, source):
-	  self.pipeline_array[-1].print_caps()
+    for p in self.pipeline_array:
+      p.print_caps()
 
 try :
 	print "\ncreating Streaming Server \n\npress Ctrl-C to exit\n"
-	m = BaseStreaming()
+	m = StreamingApplication()
 	m.run()
 except KeyboardInterrupt :
 	m.quit()
